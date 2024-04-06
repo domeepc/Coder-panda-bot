dff6fcbf480ca4b85eed565eef36b6e69a0204c4
domeepc <domagoj.milardovic@gmail.com>
2024-04-06 21:13:06 +0200

    s

--- src/shard.ts
+++ src/shard.ts
@@ -10,7 +10,7 @@ const manager = new ShardingManager(`${__dirname}/index.js`, {
 });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 manager.on('shardCreate', (shard) => {
 	console.log(`Launched shard ${shard.id}`);
