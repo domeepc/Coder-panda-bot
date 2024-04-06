32b7674b9745e893dcb1862c4c01361bf299cc09
domeepc <domagoj.milardovic@gmail.com>
2024-04-06 20:43:13 +0200

    prominia port

--- src/shard.ts
+++ src/shard.ts
@@ -1,31 +1,31 @@
-import { ShardingManager } from "discord.js";
-import config from "./config";
-import express from "express";
+import { ShardingManager } from 'discord.js';
+import config from './config';
+import express from 'express';
 
 const manager = new ShardingManager(`${__dirname}/index.js`, {
-  totalShards: "auto",
-  shardList: "auto",
-  mode: "process",
-  token: config.DISCORD_TOKEN,
+	totalShards: 'auto',
+	shardList: 'auto',
+	mode: 'process',
+	token: config.DISCORD_TOKEN,
 });
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-manager.on("shardCreate", (shard) => {
-  console.log(`Launched shard ${shard.id}`);
+manager.on('shardCreate', (shard) => {
+	console.log(`Launched shard ${shard.id}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Server started with typescript.");
+app.get('/', (req, res) => {
+	res.send('Server started with typescript.');
 });
 
 try {
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+	app.listen(port, () => {
+		console.log(`Server running at http://localhost:${port}`);
+	});
 } catch (error) {
-  console.log(error);
+	console.log(error);
 }
 
 manager.spawn({ timeout: -1 });
